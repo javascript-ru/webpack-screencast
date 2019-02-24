8fa8a94b4b913a9be89fa55d207b2d42e4aef335
Igor_Fedyukin <igor_fedyukin@epam.com>
2019-02-24 21:54:52 +0300

    Fix config, close #6

--- webpack-dll.config.js
+++ webpack-dll.config.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const webpack = require('webpack');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
 const { commonConfig, resolve } = require('./webpack-common.config');
 
 module.exports = {
